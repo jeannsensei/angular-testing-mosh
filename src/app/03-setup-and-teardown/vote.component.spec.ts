7527796c67a4dc8c0ab4a0b491cd5323af5c2b87
Jeannuel Garcia <jjeanlink@gmail.com>
2021-07-07 16:46:30 -0500

    6. Setup and Tear Down

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,28 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  it('', () => {
+  // Arrange - where we initialize the system for the test
+  let component: VoteComponent;
+  // To have each suite of testing isolated from the other
+  // we use beforeEach to clear the data used
+  // beforeEach -> set up
+  // afterEach -> tear down
+  // beforeAll ->
+  // afterAll ->
+  beforeEach(() => {
+    component = new VoteComponent();
+  });
+  it('should increment totalVotes when upvoted', () => {
+    // Act - calling a function
+    component.upVote();
+    // Assertion
+    expect(component.totalVotes).toBe(1);
   });
 
-  it('', () => {
+  it('should decrement totalVotes when downvoted', () => {
+    // Act - calling a function
+    component.downVote();
+    // Assertion
+    expect(component.totalVotes).toBe(-1);
   });
 });
